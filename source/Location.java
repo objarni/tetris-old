007307b9940914ba51471a0d0fda86fdff6b871e
Olof Bjarnason <olof.bjarnason@gmail.com>
2022-12-25 07:39:46 +0100

    Anonymize code a little more

--- source/Location.java
+++ source/Location.java
@@ -1,26 +1,4 @@
-package source;/*
- * AP(r) Computer Science GridWorld Case Study:
- * Copyright(c) 2002-2006 College Entrance Examination Board
- * (http://www.collegeboard.com).
- *
- * This code is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation.
- *
- * This code is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * @author Alyce Brady
- * @author Chris Nevison
- * @author APCS Development Committee
- * @author Cay Horstmann
- */
-
-//PACKAGE REMOVED FOR BLOCK/TETRIS LAB (DF)
-//package info.gridworld.grid;
-
+package source;
 /**
  * A <code>source.Location</code> object represents the row and column of a location
  * in a two-dimensional grid. <br />
